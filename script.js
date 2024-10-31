affeafd6f9172ccac68081bfdf38dd491eb6e11c
Asahi <163096912+asahitan@users.noreply.github.com>
2024-10-31 17:19:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // List of links with title, description, text, and URLs
 const links = [
-    { title: "Example Link 1", description: "This link takes you to Example 1.", text: "Visit Example 1", url: "https://www.example1.com" },
+    { title: "4th monthsary", description: " I’m grateful for every moment we share, for every laugh, every memory, and even our little fights. Through all of it, my love only grows stronger. I can’t wait to spend many more months, years, and lifetimes with you.", text: "4th monthsary link", url: "https://asahitan.github.io/4th-monthsary/" },
     { title: "Example Link 2", description: "Explore more on Example 2's page.", text: "Visit Example 2", url: "https://www.example2.com" },
     { title: "Example Link 3", description: "Learn about what Example 3 offers.", text: "Visit Example 3", url: "https://www.example3.com" },
     { title: "Example Link 4", description: "Find out more at Example 4.", text: "Visit Example 4", url: "https://www.example4.com" }
